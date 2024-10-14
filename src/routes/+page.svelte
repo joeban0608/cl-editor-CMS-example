f31518a66b8def75260208a6c34a09730d7cad9a
joeban <joeanddaneil5@gmail.com>
2024-10-14 19:43:18 +0800

    feat: add cl-editor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+	import Editor from 'cl-editor';
+
+	// Define the types for the editor and HTML content
+	let html: string = '<h3>hello</h3>';
+	let editor: Editor | undefined; // editor is initially undefined
+
+	// Handle the change event with the correct type for evt.detail
+	const handleEditorChange = (evt: CustomEvent<string>) => {
+		html = evt.detail;
+	};
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Editor {html} on:change={handleEditorChange} />
+
+<h1>Preview:</h1>
+{@html html}
